6d339670788a8a39152c878fa32b492bbfc4cec8
JLopes2021 <jeffinho.je8@gmail.com>
2023-06-22 03:23:51 -0300

    analise-dados-ativo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+interface Expense {
+  title: string;
+  valor: number;
+  date: string;
+  category: string;
+}
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +15,75 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  handlerMessage = '';
+  roleMessage = '';
 
+  expenses: Expense[] = [
+    { title: 'Lanches', valor: 15, date: '2023-06-20', category: 'comida' },
+    { title: 'Gas', valor: 50, date: '2023-06-19', category: 'transporte' },
+    { title: 'Aluguel', valor: 1000, date: '2023-06-18', category: 'casa-decoracao' },
+    // Add more sample expenses
+  ];
+
+  filteredExpenses: Expense[] = [];
+  searchTerm: string = '';
+  selectedCategory: string = '';
+  salary: number = 1300;
+  newExpenseTitle: string = '';
+  newExpenseAmount: number = 0;
+  newCategory: string = '';
+
+  constructor(private alertController: AlertController) {
+    this.filteredExpenses = this.expenses;
+  }
+
+  filterExpenses() {
+    this.filteredExpenses = this.expenses.filter(expense => {
+      const matchesSearchTerm = expense.title.toLowerCase().includes(this.searchTerm.toLowerCase());
+      const matchesCategory = this.selectedCategory ? expense.category === this.selectedCategory : true;
+      return matchesSearchTerm && matchesCategory;
+    });
+  }
+
+  get totalExpenses(): number {
+    return this.filteredExpenses.reduce((total, expense) => total + expense.valor, 0);
+  }
+
+  get remainingAmount(): number {
+    const remaining = this.salary - this.totalExpenses;
+    return remaining;
+  }
+
+  addExpense() {
+    const newExpense: Expense = {
+      title: this.newExpenseTitle,
+      valor: this.newExpenseAmount,
+      date: new Date().toISOString(),
+      category: this.newCategory,
+    };
+
+    this.expenses.push(newExpense);
+    this.filterExpenses();
+
+    // Reset the input fields
+    this.newExpenseTitle = '';
+    this.newExpenseAmount = 0;
+  }
+
+
+  async showExceededSalaryAlert() {
+    const alert = await this.alertController.create({
+      header: 'Gastou mais do que ganha! Enfim, eis o Brasil!',
+      message: 'Por favor, reveja as suas despesas.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  ionViewDidEnter() {
+    if (this.totalExpenses > this.salary) {
+      this.showExceededSalaryAlert();
+    }
+  }
 }
